1be77fca1a6ceb6e6ebcd3566e2ce1f400fe9d79
Ratchawit Srithongdee <ratchawit.s@cdg.co.th>
2023-03-15 22:53:23 +0700

    Add Roboto and weather color variables to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'weather-primary': '#00668a',
+        'weather-secondary': '#004e71'
+      }
+    },
+    fontFamily: {
+      Roboto: ['Roboto, sans-serif']
+    },
+    container: {
+      padding: '2rem',
+      center: true
+    },
+    screens: {
+      sm: '640px',
+      md: '768px'
+    }
   },
   plugins: []
 }
